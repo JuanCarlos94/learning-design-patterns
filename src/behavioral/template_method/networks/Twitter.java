c7998bd7212802c48cbae06620665907a90a022b
juancarlos <juancarloslsantos94@gmail.com>
2021-01-10 17:24:42 -0300

    Implementation of the Template Method design pattern

---
+++ src/behavioral/template_method/networks/Twitter.java
@@ -0,0 +1,52 @@
+package behavioral.template_method.networks;
+
+public class Twitter extends Network {
+
+    public Twitter(String userName, String password){
+        this.username = userName;
+        this.password = password;
+    }
+
+    @Override
+    boolean logIn(String username, String password) {
+        System.out.println("\nChecking user's parameters");
+        System.out.println("Name: " + this.username);
+        System.out.println("Password: ");
+        for(int i = 0; i < this.password.length(); i++){
+            System.out.println("*");
+        }
+        simulateNetworkLatency();
+        System.out.println("\nLogIn success on Twitter");
+        return true;
+    }
+
+    @Override
+    boolean sendData(byte[] data) {
+        boolean messagePosted = true;
+        if(messagePosted){
+            System.out.println("Message: '" + new String(data) + "' was posted on Twitter.");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    void logOut() {
+        System.out.println("User: '" + username + "' was logged out from Twitter");
+    }
+
+    private void simulateNetworkLatency(){
+        try{
+            int i = 0;
+            System.out.println();
+            while(i < 10){
+                System.out.println(".");
+                Thread.sleep(500);
+                i++;
+            }
+        } catch(InterruptedException ex){
+            ex.printStackTrace();
+        }
+    }
+}
