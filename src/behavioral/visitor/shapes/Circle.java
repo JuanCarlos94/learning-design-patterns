e4902437f2b5d72f05f5b4feb4b0443150d1493d
juancarlos <juancarloslsantos94@gmail.com>
2021-01-11 21:50:23 -0300

    Implementation of the Visitor design pattern

---
+++ src/behavioral/visitor/shapes/Circle.java
@@ -0,0 +1,31 @@
+package behavioral.visitor.shapes;
+
+import behavioral.visitor.visitor.Visitor;
+
+public class Circle extends Dot {
+    private int radius;
+
+    public Circle(int id, int x, int y, int radius) {
+        super(id, x, y);
+        this.radius = radius;
+    }
+
+    @Override
+    public void move(int x, int y) {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public String accept(Visitor visitor) {
+        return visitor.visitCircle(this);
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
---
+++ src/behavioral/visitor/visitor/Visitor.java
@@ -0,0 +1,12 @@
+package behavioral.visitor.visitor;
+
+
+import behavioral.visitor.shapes.*;
+
+public interface Visitor {
+    String visitDot(Dot dot);
+    String visitCircle(Circle circle);
+    String visitRectangle(Rectangle rectangle);
+    String visitCompoundGraphic(CompoundShape cg);
+    String export(Shape... shapes);
+}
